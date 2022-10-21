255f75633334b3ad0b82d56f1570eaa08fd62967
vanhiep <112918947+vanhiepfptu@users.noreply.github.com>
2022-10-22 00:19:42 +0700

    Update EquationSolvingTests.cs
    Update LinearEquation 2 more test

--- EquationSolvingTests/EquationSolvingTests.cs
+++ EquationSolvingTests/EquationSolvingTests.cs
@@ -133,6 +133,18 @@ namespace EquationSolvingTests
             {
                 Assert.AreEqual(new double[] {-2}, Program1.LinearEquationSolve(-2,3));
             }
+            
+            [Test]
+            public void LinearEquationSolveGivenWrongArguments()
+            {
+                Assert.AreNotEqual(new double[] { 2 }, Program1.LinearEquationSolve(2, 3));
+            }
+
+            [Test]
+            public void LinearEquationSolveGivenWrongArgumentsInNpercents()
+            {
+            Assert.That(new double[] {2.23}, Is.EqualTo( Program1.LinearEquationSolve(2.123, 3.92813 )).Within(1.5));
+            }
         }
 
 
